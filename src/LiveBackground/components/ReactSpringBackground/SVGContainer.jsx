9edacc11301414a2d703867cfbc1e1e3ba5c7ea8
moehuhu <994458697@qq.com>
2023-03-01 22:08:47 +0800

    使用raf重构动画

--- src/LiveBackground/components/ReactSpringBackground/SVGContainer.jsx
+++ src/LiveBackground/components/ReactSpringBackground/SVGContainer.jsx
@@ -1,10 +1,57 @@
+import { useState } from 'react'
+import { useRafInterval } from 'ahooks'
 import _ from 'lodash'
 import { ReactSVG } from 'react-svg'
-import { useSpring, animated } from '@react-spring/web'
-import { motion } from "framer-motion"
+import { motion, useAnimationFrame } from "framer-motion"
 
 const SVGContainer = (props) => {
-    const animatedWrapper = (component) => <motion.div {...props}>{component}</motion.div>
+    const { style, initial } = props
+    const {
+        initialX,
+        initialY,
+        initialRotate,
+        initialScale,
+        initialOpacity,
+        delta
+    } = initial
+    const [x, setX] = useState(initialX)
+    const [y, setY] = useState(initialX)
+    const [rotate, setRotate] = useState(initialRotate)
+    const [scale, setScale] = useState(initialScale)
+    const [opacity, setOpacity] = useState(initialOpacity)
+    const [randomNumber, setRandomNumber] = useState(_.random(0.1, 0.5, true))
+    const [ratio, setRatio] = useState(_.random(0.001, 0.005, true))
+    const [minus, setMinus] = useState(_.random(-1, 1))
+    const [clock, setClock] = useState(0)
+    useAnimationFrame(time => setClock(time))
+    useRafInterval(() => {
+        const newX = (x + randomNumber * delta)
+        const newY = initialY + Math.sin(clock * ratio) * randomNumber * minus
+        const newRotate = rotate + Math.sin(clock * ratio) * randomNumber * minus
+        const newScale = scale + Math.sin(clock * ratio) * randomNumber * minus * ratio
+
+        setX(newX > 110 ? (100 - x) : newX)
+        setY(newX > 110 ? _.random(-10, 110, true) : newY)
+        setRotate(newX > 110 ? _.random(-15, 15, true) : newRotate)
+        setScale(newX > 110 ? _.random(0.4, 1.4, true) : newScale)
+        
+        if (newY < 110) { return }
+        setRandomNumber(_.random(0.1, 0.5, true))
+        setRatio(_.random(0.001, 0.005, true))
+        setMinus(_.random(-1, 1))
+        setOpacity(_.random(0.1, 1, true))
+
+
+    }, 10)
+    const newStyle = {
+        ...style,
+        x: x + 'vw',
+        y: y + 'vh',
+        rotate,
+        scale,
+        opacity,
+    }
+    const animatedWrapper = (component) => <motion.div {...props} style={newStyle}>{component}</motion.div>
     return animatedWrapper(<ReactSVG src={props.src} />)
 }
 export default SVGContainer
--- src/LiveBackground/components/ReactSpringBackground/index.jsx
+++ src/LiveBackground/components/ReactSpringBackground/index.jsx
@@ -11,6 +11,7 @@ import crystal from '../../../assets/svgs/晶体.svg'
 import mushroom from '../../../assets/svgs/蘑菇.svg'
 import tree from '../../../assets/svgs/树.svg'
 import sleep from '../../../assets/svgs/睡眠.svg'
+import sun from '../../../assets/svgs/太阳.svg'
 import question from '../../../assets/svgs/问号.svg'
 import wave from '../../../assets/svgs/线性波浪.svg'
 import bridge from '../../../assets/svgs/小桥.svg'
@@ -21,57 +22,39 @@ import SVGContainer from './SVGContainer'
 const RenderSpringCanvas = (props) => {
     const blue = "#8abff4"
     const night = "#461d6b"
-    const count = 200
+    const count = 80
     const [svgList, setSVGList] = useState([
-        cup,
-        eat,
-        code,
-        crystal,
-        mushroom,
-        tree,
         sleep,
-        question,
-        wave,
-        bridge,
-        bottle,
-        walk,
+        sun,
         cloud1,
         cloud2,
         cloud3
     ])
     const [indexList, setIndexList] = useState(Array.from({ length: count }, (item, i) => i % _.size(svgList)))
-    const getCloudInstanceList = indexList=>indexList?.map((item, i) => {
+    const getCloudInstanceList = indexList => indexList?.map((item, i) => {
         const src = svgList[item]
-        const initialX = _.random(-40, -20, true)
+        const initialX = _.random(-10, 110, true)
         const initialY = _.random(-10, 110, true)
-        const initialRotation = _.random(-15, 15, true)
-        const initialScale = _.random(0.01, 1.4, true)
+        const initialRotate = _.random(-15, 15, true)
+        const initialScale = _.random(0.4, 1.4, true)
         const initialOpacity = _.random(0.1, 1, true)
-
-        const endX = _.random(120, 140, true)
-        const endY = initialY + _.random(-20, 20)
-        const endRotation = _.random(-15, 15, true)
-        const endScale = _.random(0.01, 1.4, true)
+        const delta = _.random(0.05, 0.2, true)
 
         const style = {
             float: 'left',
             position: 'absolute',
         }
         const initial = {
-            x: initialX + 'vw',
-            y: initialY + 'vh',
-            rotate: initialRotation,
-            scale: initialScale,
-            opacity: initialOpacity
+            initialX,
+            initialY,
+            initialRotate,
+            initialScale,
+            initialOpacity,
+            delta
         }
-        const animate = {
-            x: endX + 'vw',
-            y: endY + 'vh',
-            rotate: endRotation,
-            scale: endScale
-        }
+        const animate = {}
         const transition = {
-            duration: _.random(20, 220, true),
+            duration: _.random(1, 100, true),
             repeat: Infinity
         }
         const key = item + '-' + i
