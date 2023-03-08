101dc9d72897975725bfb45adb12bb583c61bc76
moehuhu <994458697@qq.com>
2023-03-08 16:59:39 +0800

    断桥残雪

--- src/hooks/useWeather.jsx
+++ src/hooks/useWeather.jsx
@@ -4,7 +4,7 @@ import _ from 'lodash'
 
 const useWeather = (props) => {
   const [weather, setWeather] = useState({})
-  const position = props?.position || '120.1513,30.2581'
+  const position = props?.position || '120.1513,30.2581' //断桥残雪
   const weatherAPI = `/weather/${position}/realtime`
   const fetchWeather = () => fetch(weatherAPI).then(response => response.json())
   const getWeather = _.debounce(() => fetchWeather().then(setWeather))
