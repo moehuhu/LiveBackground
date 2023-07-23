fb067e72c9a387312d06b4adce95d71f852b5c95
DESKTOP-7LHIHU9\99445 <994458697@qq.com>
2023-07-24 04:34:31 +0800

    vite env

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import requireTransform from 'vite-plugin-require-transform';
-const { env } = process
-console.log(env);
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), requireTransform.default({})],
-  assetsInclude: ['**/*.gltf', '**/*.glb'],
-  server: {
-    port: 3000,
-    proxy: {
-      '/weather': {
-        target: `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}`,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/weather/, ''),
-        configure: (proxy, options) => {
-          // proxy 是 'http-proxy' 的实例
-        }
-      },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(env);
+  return {
+    plugins: [react(), requireTransform.default({})],
+    assetsInclude: ['**/*.gltf', '**/*.glb'],
+    server: {
+      port: 3000,
+      proxy: {
+        '/weather': {
+          target: `https://api.caiyunapp.com/v2.6/${env?.WEATHER_TOKEN}`,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/weather/, ''),
+          configure: (proxy, options) => {
+            // proxy 是 'http-proxy' 的实例
+          }
+        },
+      }
     }
   }
 })
